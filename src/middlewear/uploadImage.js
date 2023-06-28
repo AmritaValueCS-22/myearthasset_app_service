316453c7da9233955c43438ded52d7cb2b34e640
Arunprabhuk <acctprabhuk@gmail.com>
2023-06-28 11:51:44 +0530

    new chnages

--- src/middlewear/uploadImage.js
+++ src/middlewear/uploadImage.js
@@ -1,33 +1,33 @@
-// import multer from "multer";
-// import path from "path";
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "uploads");
+import multer from "multer";
+import path from "path";
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      `${file.fieldname}-myEarth-${Date.now()}${path.extname(
+        file.originalname
+      )}`
+    );
+  },
+});
+const upload = multer({ storage });
+export const uploadMiddlewear = upload.single("image");
+// import multer, { diskStorage } from "multer";
+// const storage = diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./uploads");
 //   },
-//   filename: (req, file, cb) => {
-//     cb(
-//       null,
-//       `${file.fieldname}-myEarth-${Date.now()}${path.extname(
-//         file.originalname
-//       )}`
-//     );
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + "_" + file.originalname);
 //   },
 // });
-// const upload = multer({ storage });
-// export const uploadMiddlewear = upload.single("image");
-import multer, { diskStorage } from "multer";
-const storage = diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "_" + file.originalname);
-  },
-});
 
-const upload = multer({
-  storage: storage,
-  //   limits: { fileSize: 10 },
-});
+// const upload = multer({
+//   storage: storage,
+//   //   limits: { fileSize: 10 },
+// });
 
-export default upload;
+// export default upload;
